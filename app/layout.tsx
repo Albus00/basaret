508260dfb3216ef26dc6d490fb0c4226dddb44bf
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2023-05-23 21:14:05 +0200

    Started implementing new mobile menu (old one did not work)

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Header />
-        {/* <MobileMenu /> */}
+        <MobileMenu />
         <div>
           {children}
         </div>
--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,25 +1,11 @@
+'use client'
+
 import React from 'react'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import MenuItem from './MenuItem'
 
-type Props = {
-  menuOnClick: Function
-  slideMenu: boolean
-  navigateToPage: Function
-}
+const MobileMenu = () => {
 
-const MobileMenu = (props: Props) => {
-  let translationStyle = '';
-
-  // Used for the sliding animation. Component starts outside the viewport and when activated slides in
-  if (props.slideMenu) {
-    translationStyle = " translate-x-0";
-  }
-  else {
-    translationStyle = " translate-x-full";
-  }
-
-  console.log(props.slideMenu)
   return (
     <div className={"fixed top-0 right-0 h-screen w-10/12 bg-background transition-all ease-in-out shadow-2xl" + translationStyle} >
       <div className="flex flex-col p-4 space-y-5">
@@ -27,26 +13,18 @@ const MobileMenu = (props: Props) => {
         <MenuItem
           title={"Hem"}
           redirect={"/"}
-          navigateToPage={props.navigateToPage}
-          menuOnClick={props.menuOnClick}
         />
         <MenuItem
           title={"Aktivitetsschema"}
           redirect={"aktivitet"}
-          navigateToPage={props.navigateToPage}
-          menuOnClick={props.menuOnClick}
         />
         <MenuItem
           title={"Schema Basår"}
           redirect={"https://cloud.timeedit.net/liu/web/schema/ri167XQQ597Z50Qm37070gZ6y5Y7700Q6Y93Y7.html"}
-          navigateToPage={props.navigateToPage}
-          menuOnClick={props.menuOnClick}
         />
         <MenuItem
           title={"Nolleboken"}
           redirect={"nolleboken"}
-          navigateToPage={props.navigateToPage}
-          menuOnClick={props.menuOnClick}
         />
       </div>
     </div>
