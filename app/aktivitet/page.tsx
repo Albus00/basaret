16d993821d7f648156c2ba03fcc31ea76db79e51
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2023-06-29 21:09:18 +0200

    Minor fix :)

--- app/aktivitet/page.tsx
+++ app/aktivitet/page.tsx
@@ -10,15 +10,19 @@ export default function Page() {
   return (
     <main className="bg-slate-100 flex flex-col items-center pb-10 flex-1">
       <h1 className="py-10">Aktivitetsschema</h1>
-      <div className="bg-background flex items-center px-8 py-4 rounded-2xl mb-10">
+      <div
+        className="
+          bg-background flex items-center px-8 py-4 rounded-2xl mb-10
+          mx-3
+          ">
         <Image
           src={'/images/misc/alcohol.png'}
           width={40}
           height={40}
           alt="Alkoholfri"
-          className='object-contain ml-2'
+          className='object-contain mobile:w-8'
         />
-        <p className="text-2xl"> &nbsp;= Alkoholfri aktivitet</p>
+        <p className="text-2xl mobile:text-lg"> &nbsp;= Alkoholfri aktivitet</p>
       </div>
 
       {/* Generate character cards from the stories JSON file */}
