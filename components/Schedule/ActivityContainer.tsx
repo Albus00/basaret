0bee16d3dcac2181e3407cfac016110b2925f15d
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2023-06-29 21:59:37 +0200

    Added back location and time to activity

--- components/Schedule/ActivityContainer.tsx
+++ components/Schedule/ActivityContainer.tsx
@@ -11,10 +11,13 @@ type Props = {
 
 function ActivityContainer(props: Props) {
   return (
-    <div className='pl-10 mobile:pl-4'>
-      <div className='flex items-center mt-4' >
+    <div className='pl-10 mt-4 mobile:pl-4'>
+      <h6>{props.time}</h6>
+      <h5>{props.location}</h5>
+      <div className='flex items-center' >
         <h4>{props.header}</h4>
         {!props.alcohol ? (
+          // TODO: Fix alcohol icon placement
           <Image
             src={'/images/misc/alcohol.png'}
             width={24}
