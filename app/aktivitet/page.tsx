c40da920c6d3099ddf2930c12e05974236258ed8
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2023-08-28 12:50:57 +0200

    Fixed activity renderer

--- app/aktivitet/page.tsx
+++ app/aktivitet/page.tsx
@@ -4,9 +4,6 @@ import DayContainer from "@/components/Schedule/DayContainer";
 import { schedule } from "@/data/schedule_data";
 
 export default function Page() {
-  const date = new Date();
-  const currentDate = date.getDay() + "/" + date.getMonth();
-
   return (
     <main className="bg-slate-100 flex flex-col items-center pb-10 flex-1">
       <h1 className="py-10">Aktivitetsschema</h1>
@@ -31,7 +28,6 @@ export default function Page() {
         return (
           <DayContainer
             key={day.date}
-            currentDate={currentDate}
             date={day.date}
             color={day.color}
             weekday={day.weekday}
